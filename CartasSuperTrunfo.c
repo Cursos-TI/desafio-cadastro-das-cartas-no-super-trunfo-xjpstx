f03f8c16e2c76a49f2c7011704c39fc7d1d734c2
xjpstx <jpb22305@gmail.com>
2025-02-05 20:31:06 +0000

    atualização do desafio mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,50 +1,94 @@
 #include <stdio.h>
 #include <string.h>
 
-    char letra; // letra do estado (a ser utilizado no código da carta)
-    int numero; // número da cidade (a ser utilizado no código da carta)
-    int populacao, pturistico; // informações da cidade (população e os pontos turísticos)
-    char estado[100]; // informações da carta (nome do estado)
-    char cidade[100]; // informações da carta (nome da cidade)
-    float dpopulacional, pibcapita, pib, area; // densidade populacional, PIB per capita, PIB e área da cidade
+    char letra1, letra2; 
+    // letra do estado (a ser utilizado no código da carta)
+    int numero1, numero2; 
+    // número da cidade (a ser utilizado no código da carta)
+    int populacao1, populacao2, pturistico1, pturistico2; 
+    // informações da cidade (população e os pontos turísticos)
+    char estado1[100], estado2[100]; 
+    // informações da carta (nome do estado)
+    char cidade1[100], cidade2[100]; 
+    // informações da carta (nome da cidade)
+    float dpopulacional1, pibcapita1, pib1, area1, dpopulacional2, pibcapita2, pib2, area2; 
+    // densidade populacional, PIB per capita, PIB e área da cidade
+    float superpoder1, superpoder2; 
+    // soma de todas as propriedades da carta 1 e da carta 2 
 
-void geral(){
+void geral1(){
 
-    printf("Digite o nome do estado: "); 
+    printf("Digite o nome do estado da primeira carta: \n"); 
     // o usuário é solicitado a enviar o nome do estado
-    fgets(estado, sizeof(estado), stdin);
-    estado[strcspn(estado, "\n")] = 0;
+    fgets(estado1, sizeof(estado1), stdin);
+    estado1[strcspn(estado1, "\n")] = 0;
 
-    printf("Digite o nome da cidade: "); 
+    printf("Digite o nome da cidade da primeira carta: \n"); 
     // o usuário é solicitado a enviar o nome da cidade
-    fgets(cidade, sizeof(cidade), stdin);
-    cidade[strcspn(cidade, "\n")] = 0;
+    fgets(cidade1, sizeof(cidade1), stdin);
+    cidade1[strcspn(cidade1, "\n")] = 0;
 
-    printf("\nDigite o código da carta na ordem letra (do estado) e número (da cidade): \n"); 
+    printf("Digite o código da primeira carta na ordem letra (do estado) e número (da cidade): \n"); 
     // o usuário é solicitado a colocar o código no formato de letra e número (A1, A2, etc)
-    scanf(" %c %d", &letra, &numero);
+    scanf(" %c %d", &letra1, &numero1);
 
     printf("Insira a população e os pontos turísticos da sua cidade: \n"); 
     // o usuário é solicitado a colocar informações da população e da quantidade de pontos turísticos
-    scanf("%d %d", &populacao, &pturistico);
+    scanf("%d %d", &populacao1, &pturistico1);
 
     printf("Insira a área e o PIB da sua cidade: \n"); 
     // o usuário é solicitado a colocar informações da área e do PIB
-    scanf("%f %f", &area, &pib);
+    scanf(" %f %f", &area1, &pib1);
+    getchar();
+}
+
+void geral2(){
+
+    printf("\nDigite o nome do estado da segunda carta: \n"); 
+    // o usuário é solicitado a enviar o nome do estado
+    fgets(estado2, sizeof(estado2), stdin);
+    estado2[strcspn(estado2, "\n")] = 0;
+
+    printf("Digite o nome da cidade da segunda carta: \n"); 
+    // o usuário é solicitado a enviar o nome da cidade
+    fgets(cidade2, sizeof(cidade2), stdin);
+    cidade2[strcspn(cidade2, "\n")] = 0;
+
+    printf("Digite o código da segunda carta na ordem letra (do estado) e número (da cidade): \n"); 
+    // o usuário é solicitado a colocar o código no formato de letra e número (A1, A2, etc)
+    scanf(" %c %d", &letra2, &numero2);
+
+    printf("Insira a população e os pontos turísticos da sua cidade: \n"); 
+    // o usuário é solicitado a colocar informações da população e da quantidade de pontos turísticos
+    scanf("%d %d", &populacao2, &pturistico2);
+
+    printf("Insira a área e o PIB da sua cidade: \n"); 
+    // o usuário é solicitado a colocar informações da área e do PIB
+    scanf("%f %f", &area2, &pib2);
 }
 
 void calculo(){
 
-    dpopulacional = (float)populacao / area; 
+    dpopulacional1 = (float)populacao1 / area1; 
     // cálculo da densidade populacional através da divisão da população e área da cidade
-    pibcapita = (int)populacao / pib; 
+    pibcapita1 = (int)populacao1 / pib1; 
     // cálculo do PIB per capita através da divisão da população e do PIB da cidade
+
+    dpopulacional2 = (float)populacao2 / area2; 
+    // cálculo da densidade populacional através da divisão da população e área da cidade
+    pibcapita2 = (int)populacao2 / pib2; 
+    // cálculo do PIB per capita através da divisão da população e do PIB da cidade
+
+    superpoder1 = populacao1 + pturistico1 + area1 + dpopulacional1 + pib1 + pibcapita1;
+    // soma de todas as propriedades da carta 1
+    superpoder2 = populacao2 + pturistico2 + area2 + dpopulacional2 + pib2 + pibcapita2;
+    // soma de todas as propriedades da carta 2
 }
 
-void exibicao(){
+void exibicao1(){
 
     /*Ao colocar o que se pede, o prompt exibirá as seguintes informações:
-    Código da Carta (nome)
+    Código da Carta 1 (nome)
     Nome do estado
     Nome da cidade
     População
@@ -55,17 +99,62 @@ void exibicao(){
     PIB per capita
     */
 
-     printf("\nSua carta %c%d contém: \nEstado: %s \nCidade: %s \nPopulação: %d pessoas", letra, numero, estado, cidade, populacao);
-     printf("\nÁrea: %.2f km² \nPontos turísticos: %d \nPIB: %.3f bilhões de reais", area, pturistico, pib);
-     printf("\nDensidade Populacional: %.2f pessoas/km²", dpopulacional);
-     printf("\nPIB per capita: %.0f reais", pibcapita); 
+     printf("\nSua carta %c%d contém: \nEstado: %s \nCidade: %s \nPopulação: %d pessoas", letra1, numero1, estado1, cidade1, populacao1);
+     printf("\nÁrea: %.2f km² \nPontos turísticos: %d \nPIB: %.3f bilhões de reais", area1, pturistico1, pib1);
+     printf("\nDensidade Populacional: %.2f pessoas/km²", dpopulacional1);
+     printf("\nPIB per capita: %.0f reais", pibcapita1); 
+     printf("\n----------------------------\n"); // adiciona uma linha de separação
+}
+
+void exibicao2(){
+
+    /*Ao colocar o que se pede, o prompt exibirá as seguintes informações:
+    Código da Carta 2 (nome)
+    Nome do estado
+    Nome da cidade
+    População
+    Área
+    Densidade populacional
+    Pontos turísticos
+    PIB
+    PIB per capita
+    */
+
+     printf("\nSua carta %c%d contém: \nEstado: %s \nCidade: %s \nPopulação: %d pessoas", letra2, numero2, estado2, cidade2, populacao2);
+     printf("\nÁrea: %.2f km² \nPontos turísticos: %d \nPIB: %.3f bilhões de reais", area2, pturistico2, pib2);
+     printf("\nDensidade Populacional: %.2f pessoas/km²", dpopulacional2);
+     printf("\nPIB per capita: %.0f reais", pibcapita2); 
+     printf("\n----------------------------\n"); // adiciona uma linha de separação
+}
+
+void ganhador(){
+
+    printf("\nA carta 1 é vencedora se exibir 1 \nA carta 2 é vencedora se exibir 0");
+
+    printf("\nQuem venceu em população? %d", populacao1 > populacao2);
+
+    printf("\nQuem venceu em área? %d", area1 > area2);
+
+    printf("\nQuem venceu em densidade populacional? %d", dpopulacional1 < dpopulacional2);
+
+    printf("\nQuem venceu em pontos turísticos? %d", pturistico1 > pturistico2 );
+
+    printf("\nQuem venceu em PIB? %d", pib1 > pib2);
+
+    printf("\nQuem venceu em PIB per capita? %d", pibcapita1 > pibcapita2);
+
+    printf("\nQuem venceu a partida? %d", superpoder1 > superpoder2);
+
 }
 
 int main(){
 
-    geral();
+    geral1();
+    geral2();
     calculo();
-    exibicao();
+    exibicao1();
+    exibicao2();
+    ganhador();
 
     return 0;
 }
